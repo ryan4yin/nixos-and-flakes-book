de50cbe0984cf45fa405486b3bbac3fa370783ee
Ryan Yin <xiaoyin_c@qq.com>
2023-08-05 15:05:56 +0800

    feat: generate pdf for this book, bump vitepress to the latest version

---
+++ docs/.vitepress/vitepress-pdf.config.ts
@@ -0,0 +1,30 @@
+import { defineUserConfig } from "vitepress-export-pdf";
+import config from './config'
+
+
+// convert `config.themeConfig?.sidebar` to a list of routes
+let routeOrder = [];
+const sidebar = config.locales.root.themeConfig?.sidebar;
+if (sidebar) {
+  sidebar.forEach((it) => {
+    if (it.items) {
+      it.items.forEach((subItem) => {
+        routeOrder.push(subItem.link.replace(/\.md$/, ""));
+      });
+    }
+  });
+}
+
+// print routeOrder in terminal
+console.log("routeOrder: ", routeOrder);
+
+export default defineUserConfig({
+  routePatterns: ["!/zh/**"],  // exclude zh-CN pages
+	sorter: (pageA, pageB) => {
+		const aIndex = routeOrder.findIndex(route => route === pageA.path);
+		const bIndex = routeOrder.findIndex(route => route === pageB.path);
+		const index = aIndex - bIndex;
+		// console.log(`sorter: ${pageA.path} vs ${pageB.path} = ${index}`);
+		return index;
+	},
+});
