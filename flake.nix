d88a30e973244632681f8d01441250df16c3a95d
Ryan Yin <xiaoyin_c@qq.com>
2023-08-15 15:32:28 +0800

    fix: export pdf

--- flake.nix
+++ flake.nix
@@ -33,6 +33,7 @@
           # Set Puppeteer to not download Chrome, cause it doesn't work on NixOS
           export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=1
           # Set Puppeteer to use Chromium from Nixpkgs
+          export PUPPETEER_EXECUTABLE_PATH=${pkgs.chromium.outPath}/bin/chromium
         '';
       };
     });
