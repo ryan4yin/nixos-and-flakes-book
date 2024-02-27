c7bff12ab646a05a152a3d0677ff45b17fd3dfc5
Ryan Yin <xiaoyin_c@qq.com>
2024-02-27 09:45:08 +0800

    feat: adjust devshells

--- flake.nix
+++ flake.nix
@@ -6,27 +6,33 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
-
-    flake-utils.lib.eachDefaultSystem (system:
-    let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
       overlays = [
         (self: super: rec {
           nodejs = super.nodejs_20;
           pnpm = super.nodePackages.pnpm;
-          yarn = (super.yarn.override { inherit nodejs; });
+          yarn = super.yarn.override {inherit nodejs;};
         })
       ];
-      pkgs = import nixpkgs { inherit overlays system; };
-      pkgs_chromium = import nixpkgs { inherit system; };
-    in
-    {
+      pkgs = import nixpkgs {inherit overlays system;};
+      pkgs_chromium = import nixpkgs {inherit system;};
+      packages = with pkgs; [node2nix nodejs pnpm yarn git];
+    in {
       devShells.default = pkgs.mkShell {
-        packages = with pkgs; [ node2nix nodejs pnpm yarn git];
+        inherit packages;
+
+        shellHook = ''
+          echo "node `${pkgs.nodejs}/bin/node --version`"
+        '';
+      };
+
+      devShells.export-pdf = pkgs.mkShell {
+        inherit packages;
 
         shellHook = ''
           echo "node `${pkgs.nodejs}/bin/node --version`"
