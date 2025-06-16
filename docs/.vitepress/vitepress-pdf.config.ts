108ab6e5b09075eccea636a1a5658862e1b2fb42
Ryan Yin <ryan4yin@linux.com>
2025-06-16 22:40:43 +0800

    fix: pdf-exporter - fix page sorting, add margin & header/footer (#228)

--- docs/.vitepress/vitepress-pdf.config.ts
+++ docs/.vitepress/vitepress-pdf.config.ts
@@ -17,14 +17,40 @@ if (sidebar) {
 // print routeOrder in terminal
 console.log("routeOrder: ", routeOrder)
 
+const headerTemplate = `<div style="margin-top: -0.4cm; height: 70%; width: 100%; display: flex; justify-content: center; align-items: center; color: lightgray; border-bottom: solid lightgray 1px; font-size: 10px;">
+  <span class="title"></span>
+</div>`
+
+const footerTemplate = `<div style="margin-bottom: -0.4cm; height: 70%; width: 100%; display: flex; justify-content: flex-start; align-items: center; color: lightgray; border-top: solid lightgray 1px; font-size: 10px;">
+  <span style="margin-left: 15px;" class="url"></span>
+</div>`
+
 export default defineUserConfig({
+  urlOrigin: "https://nixos-and-flakes.thiscute.world/",
+  pdfOptions: {
+    format: "A4",
+    printBackground: true,
+    displayHeaderFooter: true,
+    headerTemplate,
+    footerTemplate,
+    margin: {
+      bottom: 60,
+      left: 25,
+      right: 25,
+      top: 60,
+    },
+  },
+
   routePatterns: ["!/zh/**"], // exclude zh-CN pages
   sorter: (pageA, pageB) => {
-    const aIndex = routeOrder.findIndex((route) => route === pageA.path)
-    const bIndex = routeOrder.findIndex((route) => route === pageB.path)
-    const index = aIndex - bIndex
-    // console.log(`sorter: ${pageA.path} vs ${pageB.path} = ${index}`);
-    return index
+    // console.log("pageA: ", pageA.path, "  pageB: ", pageB.path)
+
+    // remove the locale prefix
+    const pathA = pageA.path.replace("/en/", "/")
+    const pathB = pageB.path.replace("/en/", "/")
+    // compare
+    const aIndex = routeOrder.findIndex((route) => route === pathA)
+    const bIndex = routeOrder.findIndex((route) => route === pathB)
+    return aIndex - bIndex
   },
-  urlOrigin: "https://nixos-and-flakes.thiscute.world/",
 })
